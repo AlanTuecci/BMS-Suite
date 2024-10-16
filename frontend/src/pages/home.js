709cecd8d1e66cba0149514bbd0831c55356d0c7
zakariahka <102551487+zakariahka@users.noreply.github.com>
2024-10-16 13:24:20 -0400

    fixed homepage and navbar buttons

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,8 +1,8 @@
 import Layout from "../components/layout";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import './home.css'
-import hompic from '../components/HomeScreen.png'
+import './home.css';
+import hompic from '../components/HomeScreen.png';
 
 const Home = () => {
   const authState = useSelector((state) => state.auth);
@@ -11,51 +11,20 @@ const Home = () => {
     <div className="background">
     <Layout>
       <div className="text_container">
-        <h1><b></b>Designed to Improve <br></br> The Efficency of operations</h1>
-        <p1>Revolutionizing Business Management Services. <br></br>In an All-In-One Package.</p1>
-        <br></br>
-        <button className="button_container"><NavLink to="/register" className=""> Register</NavLink></button>
-        <br></br>
-        <img className="pic_container" src={hompic} alt = ''/>
-
+        <h1>Designed to Improve <br /> The Efficiency of Operations</h1>
+        <p>Revolutionizing Business Management Services. <br /> In an All-In-One Package.</p>
+        <br />
+        <div className="action_buttons">
+          <button className="button_container">
+            <NavLink to="/register">Register</NavLink>
+          </button>
+          <button className="button_container">
+            <NavLink to="/login">Login</NavLink>
+          </button>
+        </div>
+        <br />
+        <img className="pic_container" src={hompic} alt="" />
       </div>
-
-      {/* {authState.isAuth ? (
-        <div>
-          <h2>Welcome back! You have successfully signed in and can now access the site!</h2>
-          <NavLink to="/placeholder">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              style={{ margin: "10px 10px 0 0" }}
-            >
-              Placeholder
-            </button>
-          </NavLink>
-        </div>
-      ) : (
-        <div>
-          <h2>Please sign in or create an account to get started.</h2>
-          <NavLink to="/login">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              style={{ margin: "10px 10px 0 0" }}
-            >
-              Login
-            </button>
-          </NavLink>
-          <NavLink to="/register">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              style={{ margin: "10px 10px 0 0" }}
-            >
-              Register
-            </button>
-          </NavLink>
-        </div>
-      )} */}
     </Layout>
     </div>
   );
